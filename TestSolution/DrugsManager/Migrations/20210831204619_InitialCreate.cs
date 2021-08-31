8c165480f12722096a3799d46ad9b39e199c189f
ZagirAbdrashitov <zagirko@gmail.com>
2021-09-01 01:04:43 +0300

    Add migrations

---
+++ TestSolution/DrugsManager/Migrations/20210831204619_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DrugsManager.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Drug",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ndc = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    PackSize = table.Column<int>(nullable: false),
+                    Unit = table.Column<int>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drug", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Drug");
+        }
+    }
+}
