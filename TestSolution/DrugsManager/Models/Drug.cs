002838b11f4ba289ab0a7c534a890c5ac3a4e226
ZagirAbdrashitov <zagirko@gmail.com>
2021-09-02 12:46:27 +0300

    Add model attributes

--- TestSolution/DrugsManager/Models/Drug.cs
+++ TestSolution/DrugsManager/Models/Drug.cs
@@ -1,17 +1,26 @@
-﻿namespace DrugsManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrugsManager.Models
 {
     public class Drug
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(8, MinimumLength = 8)]
         public string Ndc { get; set; }
 
+        [Required]
+        [StringLength(255, MinimumLength = 3)]
         public string Name { get; set; }
 
+        [Required]
         public int PackSize { get; set; }
 
+        [Required]
         public Unit Unit { get; set; }
 
+        [Required]
         public decimal Price { get; set; }
     }
 }
