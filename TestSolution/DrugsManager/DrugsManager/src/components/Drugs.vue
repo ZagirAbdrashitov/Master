330e5c8005ede2b91c1faaaa43d6dd73265eed07
ZagirAbdrashitov <zagirko@gmail.com>
2021-09-02 12:49:06 +0300

    Add front validation

--- TestSolution/DrugsManager/DrugsManager/src/components/Drugs.vue
+++ TestSolution/DrugsManager/DrugsManager/src/components/Drugs.vue
@@ -42,65 +42,97 @@
         </tbody>
     </table>
 
-    <div class="modal fade" id="drugModal" tabindex="-1" aria-labelledby="drugModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="drugModalLabel">{{ modalTitle }}</h5>
-                    <button type="button" class="btn btn-light mr-1" data-dismiss="modal" aria-label="Close">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square-fill" viewBox="0 0 16 16">
-                            <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z" />
-                        </svg>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="d-flex flex-row bd-highlight mb-3">
-                        <div class="p-2 w-50 bd-highlight">
-                            <div class="input-group mb-3">
-                                <span class="input-group-text">NDC</span>
-                                <input type="text" class="form-control" v-model="ndc">
-                            </div>
-                            <div class="input-group mb-3">
-                                <span class="input-group-text">Name</span>
-                                <input type="text" class="form-control" v-model="name">
-                            </div>
-                            <div class="input-group mb-3">
-                                <span class="input-group-text">Pack size</span>
-                                <input type="text" class="form-control" v-model="packSize">
-                            </div>
-                            <div class="input-group mb-3">
-                                <span class="input-group-text">Unit</span>
-                                <select class="form-select" name="unitSelect" v-model="unit">
-                                    <option v-for="(item, index) in units" :key="index" :value="{ id: index, text: item }">
-                                        {{ item }}
-                                    </option>
-                                </select>
-                            </div>
-                            <div class="input-group mb-3">
-                                <span class="input-group-text">Price</span>
-                                <input type="text" class="form-control" v-model="price">
+    <form @submit.prevent="submitAction()">
+        <div class="modal fade" id="drugModal" tabindex="-1" aria-labelledby="drugModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="drugModalLabel">{{ modalTitle }}</h5>
+                        <button type="button" class="btn btn-light mr-1" data-dismiss="modal" aria-label="Close">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square-fill" viewBox="0 0 16 16">
+                                <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="d-flex flex-row bd-highlight mb-3">
+                            <div class="p-2 w-50 bd-highlight">
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text">NDC</span>
+                                    <input type="text" class="form-control" v-model="$v.ndc.$model" @blur="$v.ndc.$touch()">
+                                    <div class="error" v-if="$v.ndc.$error">
+                                        <template v-if="!$v.ndc.validFormat">
+                                            National drug code must contain only letters or digits 8 characters length
+                                        </template>
+                                        <template v-else-if="!$v.ndc.isUnique">
+                                            Drug with this NDC already exists
+                                        </template>
+                                        <template v-else>
+                                            Field is required
+                                        </template>
+                                    </div>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text">Name</span>
+                                    <input type="text" class="form-control" v-model="$v.name.$model" @blur="$v.name.$touch()">
+                                    <div class="error" v-if="$v.name.$error">
+                                        <template v-if="!$v.name.maxLength">
+                                            Name is too long
+                                        </template>
+                                        <template v-else>
+                                            Field is required
+                                        </template>
+                                    </div>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text">Pack size</span>
+                                    <input type="text" class="form-control" v-model="$v.packSize.$model" @blur="$v.packSize.$touch()">
+                                    <div class="error" v-if="$v.packSize.$error">
+                                        <template v-if="!$v.packSize.numeric">
+                                            Numeric format required
+                                        </template>
+                                        <template v-else>
+                                            Field is required
+                                        </template>
+                                    </div>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text">Unit</span>
+                                    <select class="form-select" name="unitSelect" v-model="unit">
+                                        <option v-for="(item, index) in units" :key="index" :value="{ id: index, text: item }">
+                                            {{ item }}
+                                        </option>
+                                    </select>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text">Price</span>
+                                    <input type="text" class="form-control" v-model="price">
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" @click="createClick()" v-if="!id" class="btn btn-primary" data-dismiss="modal">
-                        Create
-                    </button>
-                    <button type="button" @click="updateClick()" v-if="id" class="btn btn-primary" data-dismiss="modal">
-                        Update
-                    </button>
+                    <div class="modal-footer">
+                        <button type="submit" @click="createClick()" v-if="!id" class="btn btn-primary" data-dismiss="modal" :disabled="$v.validationGroup.$invalid">
+                            Create
+                        </button>
+                        <button type="submit" @click="updateClick()" v-if="id" class="btn btn-primary" data-dismiss="modal" :disabled="$v.validationGroup.$invalid">
+                            Update
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </form>
 </template>
 
 <script>
     import axios from "axios";
+    import { validationMixin } from "vuelidate";
+    import { required, maxLength, numeric } from "@vuelidate/validators";
 
     export default {
         name: "Drugs",
+        mixins: [validationMixin],
         data() {
             return {
                 units: ['Small Pack', 'Medium Pack', 'Large Pack'],
@@ -114,7 +146,33 @@
                 price: 0
             }
         },
+        validations() {
+            return {
+                ndc: {
+                    required,
+                    validFormat: val => /^\w{8}$/.test(val),
+                    isUnique: this.isNdcUnique
+                },
+                name: {
+                    required,
+                    maxLength: maxLength(500)
+                },
+                packSize: {
+                    required,
+                    numeric
+                },
+                validationGroup: ['ndc', 'name', 'packSize']
+            }
+        },
         methods: {
+            submitAction() {
+                alert('Form submitted');
+            },
+            isNdcUnique = (value, vm) => {
+                if (value === '') return true;
+                if (vm.drugs) return !vm.drugs.find(item => item.Ndc == value);
+                return true;
+            },
             refreshData() {
                 axios.get("api/Drugs")
                     .then((response) => {
