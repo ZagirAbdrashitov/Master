302370373c3ed0134e2e149688715882f57a3a7c
ZagirAbdrashitov <zagirko@gmail.com>
2021-09-13 07:39:40 +0300

    Add import/export data to csv file

--- TestSolution/DrugsManager/Controllers/DrugsController.cs
+++ TestSolution/DrugsManager/Controllers/DrugsController.cs
@@ -4,6 +4,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DrugsManager.Models;
 using AutoMapper;
+using ServiceStack.Text;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using ValidationContext = System.ComponentModel.DataAnnotations.ValidationContext;
+using System;
+using System.Linq;
 
 namespace DrugsManager.Controllers
 {
@@ -25,6 +31,15 @@ namespace DrugsManager.Controllers
         public async Task<ActionResult<IEnumerable<Drug>>> GetDrug()
         {
             return await _repository.GetAllDrugs();
+        }
+
+        // GET: api/Drugs/export
+        [HttpGet("export")]
+        public async Task<IActionResult> GetDrugsAndConvertToCsv()
+        {
+            var allDrugs = await _repository.GetAllDrugs();
+            var csvContent = CsvSerializer.SerializeToCsv(allDrugs);
+            return File(Encoding.ASCII.GetBytes(csvContent), "text/csv");
         }
 
         // PUT: api/Drugs
@@ -75,6 +90,72 @@ namespace DrugsManager.Controllers
             var result = await _repository.CreateDrug(newDrug);
 
             return CreatedAtAction("GetDrug", result);
+        }
+
+        // POST: api/Drugs
+        [HttpPost("import")]
+        public async Task<IActionResult> ConvertFromCsvAndPostDrug()
+        {
+            var createdCount = 0;
+            var updatedCount = 0;
+            var invalidCount = 0;
+            var errorMessage = "";
+
+            var uploadedFile = Request.Form.Files[0];
+            var uploadedDrugs = new List<Drug>();
+            try
+            {
+                uploadedDrugs = CsvSerializer.DeserializeFromStream<List<Drug>>(uploadedFile.OpenReadStream());
+            }
+            catch
+            {
+                return BadRequest("Can't convert to drugs. Some fields has bad data");
+            }
+
+            foreach (var drug in uploadedDrugs)
+            {
+                var validationResult = new List<ValidationResult>();
+                var isValid = Validator.TryValidateObject(drug, new ValidationContext(drug), validationResult, true);
+
+                if (isValid)
+                {
+                    if (_repository.IsDrugExists(drug.Id))
+                    {
+                        var allDrugs = await _repository.GetAllDrugs();
+                        var existingDrug = allDrugs.Find(d => d.Id == drug.Id);
+                        if (existingDrug.Ndc == drug.Ndc || !_repository.IsDrugExists(drug.Ndc))
+                        {
+                            await _repository.UpdateDrug(drug);
+                            updatedCount++;
+                        }
+                        else
+                        {
+                            errorMessage += $"{Environment.NewLine}Drug with NDC [{drug.Ndc}] already exists{Environment.NewLine}";
+                            invalidCount++;
+                        }
+                    }
+                    else if (!_repository.IsDrugExists(drug.Ndc))
+                    {
+                        drug.Id = 0;
+                        await _repository.CreateDrug(drug);
+                        createdCount++;
+                    }
+                    else
+                    {
+                        errorMessage += $"{Environment.NewLine}Drug with NDC [{drug.Ndc}] already exists{Environment.NewLine}";
+                        invalidCount++;
+                    }
+                }
+                else
+                {
+                    errorMessage += $"{Environment.NewLine}Drug with NDC [{drug.Ndc}] has validation problems:{Environment.NewLine}";
+                    errorMessage += $"{string.Join(Environment.NewLine, validationResult.Select(m => m.ErrorMessage))}{Environment.NewLine}";
+                    invalidCount++;
+                }
+            }
+
+            errorMessage += $"{Environment.NewLine}{invalidCount} drugs has validation problems;{Environment.NewLine}";
+            return Content($"{errorMessage}{createdCount} drugs were created;{Environment.NewLine}{updatedCount} drugs were updated;");
         }
 
         // DELETE: api/Drugs/5
--- TestSolution/DrugsManager/DrugsManager/src/components/Data.vue
+++ TestSolution/DrugsManager/DrugsManager/src/components/Data.vue
@@ -8,6 +8,7 @@
         </div>
         <div class="inner">
             <p>Load data from file</p>
+            <input type="file" ref="importFile" style="display: none" accept=".csv" v-on:change="handleFileUpload">
             <button class="btn btn-primary" @click="importData">Import</button>
         </div>
     </div>
@@ -15,14 +16,70 @@
 </template>
 
 <script>
+    import axios from "axios";
+
     export default {
         name: "Data",
+        data() {
+            return {
+                fileData: ''
+            }
+        },
         methods: {
             exportData() {
+                axios({
+                    method: 'get',
+                    url: 'api/Drugs/export',
+                    responseType: 'blob'
+                    })
+                    .then((response) => {
+                        var currentdate = new Date();
+                        var datetime = currentdate.today() + "-" + currentdate.timeNow();
+                        const blob = new Blob([response.data]);
+                        const link = document.createElement('a');
+                        link.href = URL.createObjectURL(blob);
+                        link.download = "Drugs_list_" + datetime + ".csv";
+                        link.click();
+                        URL.revokeObjectURL(link.href);
+                    })
+                    .catch(function (error) {
+                        alert(error);
+                    });
 
+                Date.prototype.today = function () {
+                    return ((this.getDate() < 10) ? "0" : "") + this.getDate() + "." +
+                        (((this.getMonth() + 1) < 10) ? "0" : "") + (this.getMonth() + 1) + "." +
+                        this.getFullYear();
+                }
+
+                Date.prototype.timeNow = function () {
+                    return ((this.getHours() < 10) ? "0" : "") + this.getHours() + "." +
+                        ((this.getMinutes() < 10) ? "0" : "") + this.getMinutes() + "." +
+                        ((this.getSeconds() < 10) ? "0" : "") + this.getSeconds();
+                }
             },
             importData() {
+                let fileInput = this.$refs.importFile;
+                fileInput.click();
+            },
+            handleFileUpload() {
+                this.fileData = this.$refs.importFile.files[0];
 
+                let formData = new FormData();
+                formData.append('file', this.fileData);
+                axios.post("api/Drugs/import",
+                    formData,
+                    {
+                        headers: {
+                            'Content-Type': 'multipart/form-data'
+                        }
+                    })
+                    .then((response) => {
+                        alert(response.data);
+                    })
+                    .catch(function (error) {
+                        alert(error.response.data);
+                    });
             }
         }
     }
